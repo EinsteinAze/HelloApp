1a8f6d320ffad36befaeba0017c9df94255ba62b
EinsteinAze <robostein2007@gmail.com>
2026-03-11 13:10:59 +0530

    UC1 Done

--- Hello.java
+++ Hello.java
@@ -1,7 +1,5 @@
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello GitFlow!");
-    System.out.println("Hello GitFlow v1.0.0!");
-    System.out.println("Hello GitFlow v1.0.1 - Bug Fixed!");
+    System.out.println("Hello World!");
   }
 }
